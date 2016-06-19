a74e7152959c5b9d18054ae688298ca12b6fa875
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-06-19 18:13:28 -0400

    Done.

--- FISGorgeOfEternalPerilViewController.m
+++ FISGorgeOfEternalPerilViewController.m
@@ -2,6 +2,19 @@
 
 #import "FISGorgeOfEternalPerilViewController.h"
 
+@interface FISGorgeOfEternalPerilViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *AcceptYourFateTapped;
+
+@end
+
 @implementation FISGorgeOfEternalPerilViewController
+
+- (IBAction)acceptYourFateTapped:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
 
 @end
